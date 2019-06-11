04a41be286033bc02c2d58c854d13c6b4924066d
Sanket Maru <sanketmaru@gmail.com>
2019-06-11 19:38:31 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c7f96b38dfeb09a0903a.js"
+    "url": "webpack-runtime-e3e1d94a107cda4edcf2.js"
   },
   {
     "url": "styles.42c18e1459d90b70be8c.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "59e8350ff7490604b20b29504be985f9"
+    "revision": "32dd9d860d2fa6ef870096df152642c1"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
